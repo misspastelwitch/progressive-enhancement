469cef9e05f6e18310e3baf7a23b711c60a7effd
Stella <stellaclough@gmail.com>
2025-03-24 13:22:31 +0100

    Completed FAQ

---
+++ assets/index.js
@@ -0,0 +1,11 @@
+ //openklap FAQ
+ console.log("beginning")
+
+ let openmeButton = document.querySelector('.openme');
+ console.log(openmeButton)
+
+openmeButton.addEventListener('click', function() {
+    openmeButton.classList.toggle('openme');
+})
+
+ console.log("end")
